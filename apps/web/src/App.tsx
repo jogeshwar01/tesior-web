144d836699f95a6e9fa414a3a5895e5079e7f996
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-05-23 11:28:53 +0530

    web: integrate solana wallet adapter

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { AppBar } from "./components/AppBar.tsx";
+import { ConnectWalletProvider } from "./components/ConnectWalletProvider.tsx";
 
 function App() {
-  return <>
-  <AppBar />
-  hello
-  
-  </>;
+  return (
+    <ConnectWalletProvider>
+      <AppBar />
+    </ConnectWalletProvider>
+  );
 }
 
 export default App;
--- apps/web/src/components/AppBar.tsx
+++ apps/web/src/components/AppBar.tsx
@@ -1,8 +1,12 @@
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 export const AppBar = () => {
   return (
     <div className="flex justify-between border-b pb-2 pt-2">
-      <div className="text-2xl pl-4 flex justify-center pt-2">Tesior</div>
-      <div className="text-xl pr-4 pb-2 pt-2">Connect Wallet</div>
+      <div className="text-2xl pl-4 flex justify-center pt-4">Tesior</div>
+      <div className="text-xl pr-4 pb-2 pt-2">
+        <WalletMultiButton />
+      </div>
     </div>
   );
 };
