e3b2f33170162c2593dad220f06c6383b310e968
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-05-31 13:31:35 +0530

    feat: add role to each user - next auth

--- apps/next-web/app/api/auth/[...nextauth]/options.ts
+++ apps/next-web/app/api/auth/[...nextauth]/options.ts
@@ -8,7 +8,30 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      profile(profile) {
+        return {
+          ...profile,
+          role: "user",
+        };
+      },
     }),
   ],
   adapter: PrismaAdapter(prisma),
+  // need to add strategy to jwt else will get jwt from database (won't go inside jwt callback)
+  // for adapters, default is database else default is jwt
+  // session: {
+  //   strategy: "jwt",
+  // },
+  callbacks: {
+    session: async ({ session, user }) => {
+      session.user = {
+        ...session.user,
+        id: user.id,
+        // @ts-ignore
+        role: user.role,
+      };
+
+      return session;
+    },
+  },
 };
