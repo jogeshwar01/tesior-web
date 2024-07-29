1721e25db9475a58af5abc1879be5e926a754a2e
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-07-30 00:08:58 +0530

    lib: vercel analytics and speed insights

--- apps/next-web/app/layout.tsx
+++ apps/next-web/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import cx from "classnames";
 import { sfPro, inter } from "./fonts";
@@ -21,6 +23,8 @@ export default async function RootLayout({
         <TooltipProvider>
           <Toaster closeButton className="pointer-events-auto" />
           {children}
+          <VercelAnalytics />
+          <SpeedInsights />
         </TooltipProvider>
       </body>
     </html>
